49826d160d0b4481d32c49c90c603127d68efb37
Shan <1713944558@qq.com>
2023-02-11 21:51:59 +0800

    实现登录拦截器

--- hm-dianping/src/main/java/com/hmdp/interceptor/LoginInterceptor.java
+++ hm-dianping/src/main/java/com/hmdp/interceptor/LoginInterceptor.java
@@ -14,7 +14,7 @@ import java.util.Objects;
  * @description
  */
 public class LoginInterceptor implements HandlerInterceptor {
-    //controller执行之前拦截
+    //controller执行之前拦截,每次请求之前校验session,后面就不用在业务中校验登录状态了
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         //获取session
@@ -24,15 +24,15 @@ public class LoginInterceptor implements HandlerInterceptor {
             response.setStatus(401);
             return false;
         }
-        //保存到ThreadLocal
+        //拦截到了请求，把用户信息放到ThreadLocal,在后面的controller就可以拿到了
         UserHolder.saveUser((User) user);
+        //放行
         return true;
     }
 
-    //销毁信息,避免内存泄露
+    //业务执行完毕,销毁信息,避免内存泄露
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-
         //移除用户
         UserHolder.removeUser();
     }
